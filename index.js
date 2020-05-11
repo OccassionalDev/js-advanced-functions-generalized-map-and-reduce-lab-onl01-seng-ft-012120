2e57a768624271e41d57ec3f44237c12913d3d84
Erica Schimmel <ericaschimmel01@gmail.com>
2020-05-11 19:54:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ function map(array, func) {
   let newArray = [];
   
   for (let i = 0; i < array.length; i++) {
-    newArray.push(func(array[i]))
+    newArray.push(func(array[i]));
   }
   
   return newArray;
 } 
+
