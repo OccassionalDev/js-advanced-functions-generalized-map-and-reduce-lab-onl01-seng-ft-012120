2ddb08574721f7c592ce36a24d2ea458faab7980
Erica Schimmel <ericaschimmel01@gmail.com>
2020-05-11 19:53:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Add your functions here
+
+function map(array, func) {
+  let newArray = [];
+  
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(func(array[i]))
+  }
+  
+  return newArray;
+} 
