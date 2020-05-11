73d1da51899681c7c965f701183b367bdeceb2a1
Erica Schimmel <ericaschimmel01@gmail.com>
2020-05-11 20:07:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+
+function map(array, func) {
+  let newArray = [];
+  
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(func(array[i]));
+  }
+  
+  return newArray;
+} 
+
+function reduce(array, func, start) {
+  
+  // Set up total variable to return
+  // If the start is defined, set the total = start, if not, make it the first value of the array
+  let total = (!!start) ? start : array[0];
+  
+  // Iterate through
+  for (let i = (!!start) ? 0 : 1; i < array.length; i++) {
+    total = func(array[i], total);
+  }
+  
+  return total;
+}
